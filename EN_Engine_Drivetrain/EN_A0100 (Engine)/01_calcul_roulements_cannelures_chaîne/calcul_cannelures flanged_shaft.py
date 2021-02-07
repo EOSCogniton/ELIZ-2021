9e15c1fc103c839e0b28ff5041390a4215e8dbbe
MKI <martin.kawczynski@ecl18.ec-lyon.fr>
2021-02-08 00:00:18 +0100

    ajouts doc Tech de l'ingé + calculs chaîne

--- EN_Engine_Drivetrain/EN_A0100 (Engine)/01_calcul_roulements_cannelures_chaîne/calcul_cannelures flanged_shaft.py
+++ EN_Engine_Drivetrain/EN_A0100 (Engine)/01_calcul_roulements_cannelures_chaîne/calcul_cannelures flanged_shaft.py
@@ -52,28 +52,6 @@ def von_mises(sigma,tau_x,tau_y):
 
 
 
-#%% Calcul concentration de contrainte dans la gorge de l'arbre
-    
-
-C=230 #couple moteur
-
-T=8680*13/15 #traction de la chaîne
-l=0.022 #bras de levier entre point d'application de la traction et gorge
-
-d=0.021 #diamètre intérieur de la gorge
-
-Mf=T*l
-sigma=Mf*32/(pi*d**3)
-Kf=1.6
-
-Mt=C
-tau=16*Mt/(pi*d**3)
-Kt=1.5
-
-VM=((Kf*sigma)**2+3*(Kt*tau)**2)**(1/2)
-
-print('sigma_VM=',round(VM/1000000),'MPa')
-print('coefficient de sécurité',round(Re/VM*100)/100)
 
 
 
---
+++ EN_Engine_Drivetrain/EN_A0100 (Engine)/01_calcul_roulements_cannelures_chaîne/calcul_chaine.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import pi
+from scipy.optimize import fsolve, fmin, curve_fit
+from scipy.interpolate import RegularGridInterpolator
+
+#%% Calculs chaîne de transmission secondaire
+
+#%% Chaînes [pas,masse linéique, charge rupture]
+
+B08B=[12.7,0.68,17800]
+B10B=[15.875,0.85,22200]
+B12B=[19.05,1.16,28900]
+
+#%% Calcul
+
+chain=B10B
+
+p=chain[0]/1000
+M=chain[1]
+Frup=chain[2]
+
+#Pignon & moteur
+C=230
+Z=15
+omega_max=5500*2*pi/60
+
+#Couronne
+Zc=round(3.23*Z)
+
+#%% Géométrie
+
+alpha=2*pi/Z
+phi=pi*(1/6-1/Z)
+Dp= p/np.sin(pi/Z)
+
+e=0.2
+L=2*e+p*(Z+Zc)/2+p**2/e*((Z-Zc)/(2*pi))**2
+
+#%% Dynamique
+Fu= 2*C/Dp
+Fc= M*(Dp*omega_max/2)**2
+
+
+indice=np.array(range(1,Z))
+
+F=(Fu+Fc)*(np.sin(phi)/np.sin(alpha+phi))**indice
+G=(Fu+Fc)*(np.sin(phi)/np.sin(alpha+phi))**(indice-1)*(np.sin(alpha)/np.sin(alpha+phi))
+
+GX=G*np.cos(phi-alpha*(indice-1/2))
+GZ=G*np.sin(-phi+alpha*(indice-1/2))
+
+Krup=Frup/(Fu+Fc)
+
+#%% Coefficients de sécurité (d'après Chaînes mécaniques - Techniques de l'ingénieur)
+
+K1=19/Z
+K2=1
+K3=120*p/L
+K4=1.1
+K5=1
+
+K=K1*K2*K3*K4*K5
