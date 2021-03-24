4297376320399edb58a7c5d4ec68e65316112378
Théo <theo.lenoir4@orange.fr>
2021-03-24 15:33:04 +0100

    maj code python soudure

--- FR_Frame_Body/FR_A01000 (Plaque container)/soudure.py
+++ FR_Frame_Body/FR_A01000 (Plaque container)/soudure.py
@@ -1,19 +1,25 @@
 from math import *
 ## voir Fanchon pages 238 et suivantes
 
-d=20e-3 #dimension du tube
-b=d
-a=5e-3 #epaisseur de soudure
-v=d/2
-F=13e3 #force
-L=120e-3 #bras de levier
+d=3e-3 #dimension du tube carré
+b=20e-3
+a=3.3e-3 #epaisseur de soudure
+v_carre=d/2
 
+F=6.125e3/2 #force
+L=16.4e-3 #bras de levier
+rext = 28e-3 
+rint = 24e-3
+v_rond = rext/2
 # Contraintes de flexion
 M_f=F*L
-I_uz=d**2/6*(3*b+d)
-sigma_2=M_f/((sqrt(2)*a*I_uz)/v)
-to_2=sigma_2
+I_uz_carre=d**2/6*(3*b+d)
+I_uz_rond = pi*rext**3 - pi*rint**3
+sigma_2_carre=M_f/((sqrt(2)*a*I_uz_carre)/v_carre)
+to_2_carre=sigma_2_carre
 
+sigma_2_rond=M_f/((sqrt(2)*a*I_uz_rond)/v_rond)
+to_2_rond=sigma_2_rond
 # Contraintes de cisaillement
 to_1=F/(a*L)
 
@@ -22,6 +28,10 @@ to_3=F/(a*sqrt(2)*L)
 sigma_3=to_3
 
 # Contraintes de Von Mises
-sigma_sens_1=sqrt(sigma_2**2+3*(to_1**2+to_2**2))
-sigma_sens_2=sqrt((sigma_2+sigma_3)**2+3*(to_2-to_3)**2)
-print(max(sigma_sens_1,sigma_sens_2)/(10**6))
+sigma_sens_1_carre=sqrt(sigma_2_carre**2+3*(to_1**2+to_2_carre**2))
+sigma_sens_2_carre=sqrt((sigma_2_carre+sigma_3)**2+3*(to_2_carre-to_3)**2)
+print(max(sigma_sens_1_carre,sigma_sens_2_carre)/(10**6))
+
+sigma_sens_1_rond=sqrt(sigma_2_rond**2+3*(to_1**2+to_2_rond**2))
+sigma_sens_2_rond=sqrt((sigma_2_rond+sigma_3)**2+3*(to_2_rond-to_3)**2)
+print(max(sigma_sens_1_rond,sigma_sens_2_rond)/(10**6))
